6c36abc02f8575af36502dddce2739dc5ac969d3
aamna-ansari <aamnansari29@gmail.com>
2025-01-06 20:53:34 +0500

    Enhance UI components: update Header and Footer layout, add Font Awesome icons, and improve Home page messaging

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -2,8 +2,15 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
-  )
+    <footer className="footer  flex flex-col justify-center  items-center gap-3 bg-red-200 py-10 text-gray-600 ">
+      <h2 className="brand text-4xl font-semibold text-center italic ">
+        Recipe<span className=" text-red-500 ">Nest</span>
+      </h2>
+      <p className="text-gray-700">
+        &copy; 2024 RecipeNest. All rights reserved.
+      </p>
+    </footer>
+  );
 }
 
 export default Footer
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,7 +2,12 @@ import React from 'react'
 
 const Home = () => {
   return (
-    <>Home</>
+    <>
+    <p className="lg:text-4xl text-xl text-center text-red-300 font-semibold">
+            Nothing to show, please search something!
+          </p>
+
+    </>
   )
 }
 
