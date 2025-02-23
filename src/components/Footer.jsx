08fb273e7246742aa901c4c1405924f9f65f80aa
aamna-ansari <aamnansari29@gmail.com>
2025-02-23 16:40:52 +0500

    shift to tailwind Css 4

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,13 +1,14 @@
 import React from 'react'
 
 const Footer = () => {
+  const year = new Date().getFullYear();
   return (
     <footer className="flex flex-col justify-center items-center bg-red-200 py-10 text-gray-600 ">
       <h2 className="text-4xl font-semibold text-center italic ">
         Recipe<span className=" text-red-500 ">Nest</span>
       </h2>
       <p className="text-gray-700">
-        &copy; 2024 RecipeNest. All rights reserved.
+        &copy; {year} RecipeNest. All rights reserved.
       </p>
     </footer>
   );
