46803b8df13a8c86ddcade5397d9804af52f7461
aamna-ansari <aamnansari29@gmail.com>
2025-03-08 20:19:43 +0500

    Refactor Home component to clean up unused code and improve readability

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,17 +1,20 @@
-import React from 'react'
-import FryingPan from '../components/FryingPan'
-
+import React from "react";
+import FryingPan from "../components/FryingPan";
 
 const Home = () => {
   return (
     <>
-    <p className="lg:text-4xl text-xl text-center text-red-300 font-semibold mt-6">
-            Nothing to show, please search something!
-          </p>
-          <FryingPan/>
+      {/* <p className="lg:text-4xl text-xl text-center text-red-300 font-semibold mt-6">
+        Nothing to show, please search something!
+      </p>
+      <FryingPan /> */}
 
+      {/* Receipe Card */}
+      <div className="">
+        <div></div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
