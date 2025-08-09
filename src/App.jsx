6c5b437574245738189e62a25007b3aca1b0ea3c
Amna BB <theamnabb@gmail.com>
2025-08-09 11:02:13 +0500

    add app routies

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,47 @@
-import { Route, Routes } from "react-router-dom";
-import Footer from "./components/Footer";
+import React, { useState, useEffect } from "react";
+import { Routes, Route } from "react-router-dom"; // NO BrowserRouter here
 import Header from "./components/Header";
+import Footer from "./components/Footer";
 import Home from "./pages/Home";
 import Favourites from "./pages/Favourites";
+import RecipeDetail from "./pages/RecipeDetail";
 
 function App() {
+  const [favorites, setFavorites] = useState([]);
+
+  useEffect(() => {
+    const savedFavs = JSON.parse(localStorage.getItem("favorites")) || [];
+    setFavorites(savedFavs);
+  }, []);
+
+  const addToFavorites = (recipe) => {
+    if (favorites.find((fav) => fav.id === recipe.id)) {
+      alert("Recipe already in favorites!");
+      return;
+    }
+    const updated = [...favorites, recipe];
+    setFavorites(updated);
+    localStorage.setItem("favorites", JSON.stringify(updated));
+  };
+
   return (
-    <>
-      <div className="min-h-screen flex flex-col">
+    <div className="min-h-screen flex flex-col">
       <Header />
       <main className="flex-grow">
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/favourite" element={<Favourites />} />
+          <Route path="/" element={<Home addToFavorites={addToFavorites} />} />
+          <Route
+            path="/recipe/:id"
+            element={<RecipeDetail addToFavorites={addToFavorites} />}
+          />
+          <Route
+            path="/favourite"
+            element={<Favourites favorites={favorites} />}
+          />
         </Routes>
       </main>
       <Footer />
     </div>
-    </>
   );
 }
 
